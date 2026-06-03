a9c2c9a47918f8146348e8c75ffe861586163285
lailalauanny13 <lailalauanny1@outlook.com>
2026-06-03 11:49:50 -0300

    terminando

--- paginas/produtos/index.php
+++ paginas/produtos/index.php
@@ -10,6 +10,7 @@ $query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $m
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="shortcut icon" type="image/x-icon" href="icon.ico">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Larpintmax - Módulo de Produtos</title>
